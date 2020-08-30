d1f083a850de9b34f9f2dd2eb240215d81311ce3
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-30 06:09:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -41,6 +41,6 @@ def now_serving(line)
     puts"There is nobody waiting to be served!"
   else
     puts "Currently serving #{line[0]}." 
-    line.shift # this works in the IDE but no on repl.it
+    line.shift
   end
 end
